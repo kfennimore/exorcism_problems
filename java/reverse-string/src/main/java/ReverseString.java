fe41d953ffafdc36beb44f83fe7af37468d87570
Karousha Fennimore <karousha@gmail.com>
2018-04-12 14:14:06 -0400

    completed

---
+++ java/reverse-string/src/main/java/ReverseString.java
@@ -0,0 +1,11 @@
+class ReverseString {
+
+    String reverse(String inputString) {
+        StringBuilder sb = new StringBuilder();
+
+        String reversedWord = sb.append(inputString).reverse().toString();
+
+        return reversedWord;
+    }
+  
+}
